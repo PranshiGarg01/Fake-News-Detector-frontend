78f76a8a7a938061d7c03384fbf8b79cbc7c83fe
PranshiGarg9420 <garg.pranshi09590@gmail.com>
2025-06-03 11:44:02 +0530

    Updated Navbar and footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-left">
+        <img 
+          src="/images/SIAM_VIT.png"
+          alt="Footer Logo" 
+          className="footer-logo" 
+        />
+      </div>
+
+      <div className="footer-right">
+        <a href="https://instagram.com/siamvit/?hl=en" target="_blank" rel="noopener noreferrer" className="footer-icon insta">
+          <img src="/images/instagram.png" alt="Instagram" />
+        </a>
+        <a href="https://www.youtube.com/@SIAMVIT" target="_blank" rel="noopener noreferrer" className="footer-icon youtube">
+          <img src="/images/youtube.png" alt="YouTube" />
+        </a>
+        <a href="https://linkedin.com/company/siam-vit" target="_blank" rel="noopener noreferrer" className="footer-icon linkedin">
+          <img src="/images/linkedin.png" alt="LinkedIn" />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="footer-icon twitter">
+          <img src="/images/twitter.png" alt="Twitter" />
+        </a>
+        <a href="https://github.com/SIAM-VIT" target="_blank" rel="noopener noreferrer" className="footer-icon twitter">
+          <img src="/images/github.png" alt="Github" />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        LieSpy
+      </div>
+      <div className="navbar-right">
+        <Link to="/" className="navbar-link">Home</Link>
+        <Link to="/contact" className="navbar-link">Contact</Link>
+        <Link to="/aboutus" className="navbar-link">About Us</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
