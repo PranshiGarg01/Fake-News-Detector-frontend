db6acbcbf0d48e127e8060fd510604135a97f1ec
PranshiGarg9420 <garg.pranshi09590@gmail.com>
2025-06-11 11:36:44 +0530

    Footer Updated for medium

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,10 +22,10 @@ const Footer = () => {
         <a href="https://linkedin.com/company/siam-vit" target="_blank" rel="noopener noreferrer" className="footer-icon linkedin">
           <img src="/images/linkedin.png" alt="LinkedIn" />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="footer-icon twitter">
-          <img src="/images/twitter.png" alt="Twitter" />
+        <a href="https://medium.com/@siam_31894" target="_blank" rel="noopener noreferrer" className="footer-icon medium">
+          <img src="/images/medium.png" alt="Twitter" />
         </a>
-        <a href="https://github.com/SIAM-VIT" target="_blank" rel="noopener noreferrer" className="footer-icon twitter">
+        <a href="https://github.com/SIAM-VIT" target="_blank" rel="noopener noreferrer" className="footer-icon github">
           <img src="/images/github.png" alt="Github" />
         </a>
       </div>
